6aa5b37b6b02d98650c58126f9a6003a090e6c7d
Nathaniel Blalock <nathanielblalock@Nathaniels-MacBook-Air-3.local>
2025-03-04 14:16:19 -0500

    had to update aa2ind for functions.py as well, torchtext sucks

--- functions.py
+++ functions.py
@@ -274,9 +274,9 @@ def generate_and_evaluate_mutants_p_sampling(WT, reward_models, model, model_ide
     # print(mutated_seqs)
  
     # Score mutants
-    AAs = 'ACDEFGHIKLMNPQRSTVWY-' # setup torchtext vocab to map AAs to indices, usage is aa2ind(list(AAsequence))
-    aa2ind = vocab.vocab(OrderedDict([(a, 1) for a in AAs]))
-    aa2ind.set_default_index(20) # set unknown charcterers to gap
+    # AAs = 'ACDEFGHIKLMNPQRSTVWY-' # setup torchtext vocab to map AAs to indices, usage is aa2ind(list(AAsequence))
+    # aa2ind = vocab.vocab(OrderedDict([(a, 1) for a in AAs]))
+    # aa2ind.set_default_index(20) # set unknown charcterers to gap
     batch_size = num_designs
     scores_tensor = torch.zeros((len(reward_models), batch_size), dtype=torch.float32).to(device)
     for i, reward_model in enumerate(reward_models):
@@ -284,7 +284,7 @@ def generate_and_evaluate_mutants_p_sampling(WT, reward_models, model, model_ide
         reward_model.eval()
         with torch.no_grad():
             for j, seq in enumerate(mutated_seqs):
-                sequence = torch.tensor(aa2ind(list(seq))).to(device)
+                # sequence = torch.tensor(aa2ind(list(seq))).to(device)
                 score = reward_model.predict(sequence)[0][0]
                 scores_tensor[i, j] = score
 
