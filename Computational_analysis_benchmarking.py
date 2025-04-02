bfa6e5567d2a2c22b7a5175cb531cfbf0c26d7d8
Nathaniel Blalock <nathanielblalock@Nathaniels-MacBook-Air-3.local>
2025-04-02 13:31:32 -0400

    updating model locations for generating designs at mutational regimes

--- Computational_analysis_benchmarking.py
+++ Computational_analysis_benchmarking.py
@@ -349,13 +349,13 @@ for huggingface_identifier in esm2_models:
         generate_pretrained_designs = True
         generate_sft_designs = True
         sft_model_exists = True
-        sft_model_filepath = f'./logs/version_{sft_version}/{model_size}' # ! update
+        sft_model_filepath = f'./logs/version_{sft_version}' # ! update
         sft_model_name = f'SFT_{huggingface_identifier}_v0' # ! update
 
         # Generate designs from aligned model
         generate_aligned_designs = True
         rl_model_exists = True
-        rl_model_filepath = f'{filepath}/version_{version}/{model_size}' # ! update
+        rl_model_filepath = f'{filepath}/version_{version}' # ! update
         rl_model_name = f'ema_aligned_{huggingface_identifier}_v{version}_ep{ep}' # ! update
 
         ################################################################################################################
