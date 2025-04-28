aa3fa7635377d3220a08dbe7c0686848a50777be
Nathaniel Blalock <nathanielblalock@Nathaniels-MacBook-Air-3.local>
2025-04-28 09:35:28 -0400

    Initial clean commit

---
+++ Training_Ensemble_of_reward_models.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Importing Packages
+import numpy as np
+import pandas as pd
+import torch
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint
+from pytorch_lightning.loggers import CSVLogger
+import matplotlib.pyplot as plt
+import enum
+import os
+from sklearn import metrics
+import torchmetrics
+from scipy.stats import spearmanr
+import csv
+
+# import helper scripts (you can change this if you want but check out https://www.nature.com/articles/s41467-024-50712-3)
+from MLP import (SeqFcnDataset, ProtDataModule, MLP)
+
+# Parameters to update
+data_filepath = ''
+num_muts_threshold = 1 # variants with this number of mutations or less will be in training set
+num_muts_of_val_test_splits = 2 # variants with this number of mutations will be split into validation and test sets
+percent_validation_split = 75 # defines percent of variants with num_muts_of_val_test_splits mutations to be in validation set
+learning_rate = 1e-6
+batch_size = 128
+epochs = 1000
+num_models = 3 # number of models in ensemble
+patience = 200 # patience for EarlyStopping, I recommend training ensemble for awhile after loss plateaus
+WT = 'MQYKLILNGKTLKGETTTEAVDAATAEKVFKQYANDNGVDGEWTYDDATKTFTVTE' # parent sequence
+slen = len(WT) # length of parent sequence
+
+# load data
+df = pd.read_pickle(f"{data_filepath}SeqFxnDataset.pkl") # load preprocessed data with Sequence and Score column
+# df.head()
+
+# create data splits for SeqFxnDataset
+splits_path = f'.{data_filepath}/SeqFxnDataset_splits.pkl'
+# dm = ProtDataModule(df, num_muts_threshold, num_muts_of_val_test_splits, percent_validation_split, batch_size=None, splits_path=None)
+# dm.save_splits(splits_path)
+
+# load data for model
+dm = ProtDataModule(df, num_muts_threshold, num_muts_of_val_test_splits, percent_validation_split, batch_size, splits_path)
+
+# ############################################################## train reward models ##############################################################
+model_savepath = './reward_models'
+os.makedirs(model_savepath, exist_ok=True)
+
+# Check if a GPU is available and set the trainer to use it
+for i in range(num_models):
+    model = MLP(learning_rate, batch_size, epochs, slen) # Resubstantiate the model for each training iteration
+    model = model.to('cuda' if torch.cuda.is_available() else 'cpu')  # Move model to GPU if available
+    print(f"Model {i} is trained on: {model.device}")
+    
+    logger_name = f'reward_model'
+    logger = CSVLogger('logs', name=logger_name)
+    checkpoint_callback = ModelCheckpoint(dirpath=model_savepath,filename=f'reward_model_v{i}',monitor='val_loss',mode='min',save_top_k=1) # Define the model checkpoint callback with version number in the filename
+    early_stopping = EarlyStopping(monitor='val_loss', patience=patience, mode='min') # Use early stopping
+    trainer = pl.Trainer(logger=logger, max_epochs=epochs, callbacks=[early_stopping, checkpoint_callback], enable_progress_bar=False, devices="auto", accelerator="auto") # Trainer with early stopping and checkpointing
+    trainer.fit(model, dm) # Train the model
+    metrics_file_name = f'metrics.csv'
+    pt_metrics = pd.read_csv(f'logs/reward_model/version_{i}/metrics.csv')
+
+############################################################## plot loss curves ##############################################################
+train_losses = []
+val_losses = []
+for i in range(num_models):
+    try:
+        pt_metrics = pd.read_csv(f'logs/reward_model/version_{i}/metrics.csv')
+        train = pt_metrics[~pt_metrics.train_loss.isna()]
+        val = pt_metrics[~pt_metrics.val_loss.isna()]
+        max_epochs = max(len(train), len(val))  # Update the maximum number of epochs
+        train_losses.append(train.train_loss.tolist())
+        val_losses.append(val.val_loss.tolist())
+    except FileNotFoundError:
+        print(f"Metrics file for version {i} not found.")
+        continue
+
+# Pad each loss array in train_losses and val_losses to max_length with np.nan
+max_length = max(max(len(loss_array) for loss_array in train_losses), max(len(loss_array) for loss_array in val_losses))
+train_losses_padded = [np.pad(loss_array, (0, max_length - len(loss_array)), constant_values=np.nan) for loss_array in train_losses]
+val_losses_padded = [np.pad(loss_array, (0, max_length - len(loss_array)), constant_values=np.nan) for loss_array in val_losses]
+
+# Calculate the mean and standard deviation
+train_mean = np.nanmean(train_losses_padded, axis=0)
+val_mean = np.nanmean(val_losses_padded, axis=0)
+train_std = np.nanstd(train_losses_padded, axis=0)
+val_std = np.nanstd(val_losses_padded, axis=0)
+epochs = np.arange(max_length)
+
+# Plot figure
+plt.figure(figsize=(8, 6))
+plt.plot(epochs, train_mean, label='Training Loss')
+plt.fill_between(epochs, train_mean - train_std, train_mean + train_std, alpha=0.2, label='±1 Standard Deviation')
+plt.plot(epochs, val_mean, label='Validation Loss')
+plt.fill_between(epochs, val_mean - val_std, val_mean + val_std, alpha=0.2, label='±1 Standard Deviation')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+ax = plt.gca()  # Get the current axis
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+plt.legend()
+file_path = os.path.join(model_savepath, 'Loss_Curve.png')
+plt.savefig(file_path, bbox_inches='tight')
+# plt.show()
+
+############################################################## evaluate reward models on test set ##############################################################
+all_Y_values = [[] for _ in range(len(df.iloc[dm.test_idx]))]  # List of lists to store predictions for each sequence
+
+# Scores Test Sequences for Models
+for i in range(num_models):
+    model = MLP(learning_rate, batch_size, epochs, slen)
+    model_path = f'{model_savepath}/reward_model_v{i}.ckpt'
+    checkpoint = torch.load(model_path)
+    model.load_state_dict(checkpoint['state_dict'])
+    model.eval()
+    test_data_frame = df.iloc[list(dm.test_idx)].copy()  # Use the test index from DataModule
+    Y = [model.predict(j).item() for j in test_data_frame['sequence']]
+    for j, score in enumerate(Y):
+        all_Y_values[j].append(score)
+
+# Calculate median, mean, and variance for each sequence
+medians = [np.median(scores) for scores in all_Y_values]
+# variances = [np.var(scores) for scores in all_Y_values]
+
+# Calculating metrics
+actual_scores = test_data_frame['functional_score'].tolist()
+mse = metrics.mean_squared_error(actual_scores, medians)
+r = np.corrcoef(actual_scores, medians)[0][1]
+rho, _ = spearmanr(actual_scores, medians)
+
+# Plot the results
+plt.figure(figsize=(8, 6))
+plt.scatter(actual_scores, medians, color='red', s=5)
+plt.plot([min(min(actual_scores), min(medians)), max(max(actual_scores),max(medians))], [min(min(actual_scores),min(medians)), max(max(actual_scores),max(medians))], color='black')  # Diagonal line for reference
+plt.xlabel("Actual Score")
+plt.ylabel("Predicted Score")
+plt.title("Model Test Results")
+plt.text(3.0, 4.1, f"MSE = {mse:.5f}")
+plt.text(3.0, 4.05, f"R = {r :.3f}")
+plt.text(3.0, 4.0, f"Rho = {rho :.3f}")
+ax = plt.gca()  # Get the current axis
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+plot_file_path = f'{model_savepath}/Test_Results.png'
+plt.savefig(plot_file_path)
+
+# Save metrics
+metrics_data = [['MSE', 'Pearson R', "Spearman's Rho"], [mse, r, rho]]
+csv_file_path = plot_file_path.replace('.png', '.csv')
+with open(csv_file_path, mode='w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerows(metrics_data)
+
+
+
+
---
+++ logs/figures/35M/color_gb1_Aligned_ESM2.pml
@@ -0,0 +1,121 @@
+load gb1_AF3.pdb, gb1_AF3
+set_color color_gb1_AF3_0, [0.941, 0.941, 0.941]
+color color_gb1_AF3_0, /gb1_AF3//A/1
+set_color color_gb1_AF3_1, [0.889, 0.673, 0.655]
+color color_gb1_AF3_1, /gb1_AF3//A/2
+set_color color_gb1_AF3_2, [0.941, 0.941, 0.941]
+color color_gb1_AF3_2, /gb1_AF3//A/3
+set_color color_gb1_AF3_3, [0.923, 0.845, 0.839]
+color color_gb1_AF3_3, /gb1_AF3//A/4
+set_color color_gb1_AF3_4, [0.932, 0.892, 0.889]
+color color_gb1_AF3_4, /gb1_AF3//A/5
+set_color color_gb1_AF3_5, [0.941, 0.941, 0.941]
+color color_gb1_AF3_5, /gb1_AF3//A/6
+set_color color_gb1_AF3_6, [0.889, 0.673, 0.655]
+color color_gb1_AF3_6, /gb1_AF3//A/7
+set_color color_gb1_AF3_7, [0.932, 0.892, 0.889]
+color color_gb1_AF3_7, /gb1_AF3//A/8
+set_color color_gb1_AF3_8, [0.905, 0.756, 0.743]
+color color_gb1_AF3_8, /gb1_AF3//A/9
+set_color color_gb1_AF3_9, [0.905, 0.756, 0.743]
+color color_gb1_AF3_9, /gb1_AF3//A/10
+set_color color_gb1_AF3_10, [0.932, 0.892, 0.889]
+color color_gb1_AF3_10, /gb1_AF3//A/11
+set_color color_gb1_AF3_11, [0.914, 0.800, 0.790]
+color color_gb1_AF3_11, /gb1_AF3//A/12
+set_color color_gb1_AF3_12, [0.923, 0.845, 0.839]
+color color_gb1_AF3_12, /gb1_AF3//A/13
+set_color color_gb1_AF3_13, [0.923, 0.845, 0.839]
+color color_gb1_AF3_13, /gb1_AF3//A/14
+set_color color_gb1_AF3_14, [0.914, 0.800, 0.790]
+color color_gb1_AF3_14, /gb1_AF3//A/15
+set_color color_gb1_AF3_15, [0.923, 0.845, 0.839]
+color color_gb1_AF3_15, /gb1_AF3//A/16
+set_color color_gb1_AF3_16, [0.941, 0.941, 0.941]
+color color_gb1_AF3_16, /gb1_AF3//A/17
+set_color color_gb1_AF3_17, [0.897, 0.714, 0.698]
+color color_gb1_AF3_17, /gb1_AF3//A/18
+set_color color_gb1_AF3_18, [0.941, 0.941, 0.941]
+color color_gb1_AF3_18, /gb1_AF3//A/19
+set_color color_gb1_AF3_19, [0.905, 0.756, 0.743]
+color color_gb1_AF3_19, /gb1_AF3//A/20
+set_color color_gb1_AF3_20, [0.923, 0.845, 0.839]
+color color_gb1_AF3_20, /gb1_AF3//A/21
+set_color color_gb1_AF3_21, [0.932, 0.892, 0.889]
+color color_gb1_AF3_21, /gb1_AF3//A/22
+set_color color_gb1_AF3_22, [0.932, 0.892, 0.889]
+color color_gb1_AF3_22, /gb1_AF3//A/23
+set_color color_gb1_AF3_23, [0.932, 0.892, 0.889]
+color color_gb1_AF3_23, /gb1_AF3//A/24
+set_color color_gb1_AF3_24, [0.923, 0.845, 0.839]
+color color_gb1_AF3_24, /gb1_AF3//A/25
+set_color color_gb1_AF3_25, [0.914, 0.800, 0.790]
+color color_gb1_AF3_25, /gb1_AF3//A/26
+set_color color_gb1_AF3_26, [0.923, 0.845, 0.839]
+color color_gb1_AF3_26, /gb1_AF3//A/27
+set_color color_gb1_AF3_27, [0.897, 0.714, 0.698]
+color color_gb1_AF3_27, /gb1_AF3//A/28
+set_color color_gb1_AF3_28, [0.797, 0.197, 0.147]
+color color_gb1_AF3_28, /gb1_AF3//A/29
+set_color color_gb1_AF3_29, [0.932, 0.892, 0.889]
+color color_gb1_AF3_29, /gb1_AF3//A/30
+set_color color_gb1_AF3_30, [0.932, 0.892, 0.889]
+color color_gb1_AF3_30, /gb1_AF3//A/31
+set_color color_gb1_AF3_31, [0.932, 0.892, 0.889]
+color color_gb1_AF3_31, /gb1_AF3//A/32
+set_color color_gb1_AF3_32, [0.923, 0.845, 0.839]
+color color_gb1_AF3_32, /gb1_AF3//A/33
+set_color color_gb1_AF3_33, [0.932, 0.892, 0.889]
+color color_gb1_AF3_33, /gb1_AF3//A/34
+set_color color_gb1_AF3_34, [0.914, 0.800, 0.790]
+color color_gb1_AF3_34, /gb1_AF3//A/35
+set_color color_gb1_AF3_35, [0.914, 0.800, 0.790]
+color color_gb1_AF3_35, /gb1_AF3//A/36
+set_color color_gb1_AF3_36, [0.923, 0.845, 0.839]
+color color_gb1_AF3_36, /gb1_AF3//A/37
+set_color color_gb1_AF3_37, [0.923, 0.845, 0.839]
+color color_gb1_AF3_37, /gb1_AF3//A/38
+set_color color_gb1_AF3_38, [0.914, 0.800, 0.790]
+color color_gb1_AF3_38, /gb1_AF3//A/39
+set_color color_gb1_AF3_39, [0.905, 0.756, 0.743]
+color color_gb1_AF3_39, /gb1_AF3//A/40
+set_color color_gb1_AF3_40, [0.914, 0.800, 0.790]
+color color_gb1_AF3_40, /gb1_AF3//A/41
+set_color color_gb1_AF3_41, [0.914, 0.800, 0.790]
+color color_gb1_AF3_41, /gb1_AF3//A/42
+set_color color_gb1_AF3_42, [0.941, 0.941, 0.941]
+color color_gb1_AF3_42, /gb1_AF3//A/43
+set_color color_gb1_AF3_43, [0.941, 0.941, 0.941]
+color color_gb1_AF3_43, /gb1_AF3//A/44
+set_color color_gb1_AF3_44, [0.923, 0.845, 0.839]
+color color_gb1_AF3_44, /gb1_AF3//A/45
+set_color color_gb1_AF3_45, [0.905, 0.756, 0.743]
+color color_gb1_AF3_45, /gb1_AF3//A/46
+set_color color_gb1_AF3_46, [0.905, 0.756, 0.743]
+color color_gb1_AF3_46, /gb1_AF3//A/47
+set_color color_gb1_AF3_47, [0.932, 0.892, 0.889]
+color color_gb1_AF3_47, /gb1_AF3//A/48
+set_color color_gb1_AF3_48, [0.914, 0.800, 0.790]
+color color_gb1_AF3_48, /gb1_AF3//A/49
+set_color color_gb1_AF3_49, [0.923, 0.845, 0.839]
+color color_gb1_AF3_49, /gb1_AF3//A/50
+set_color color_gb1_AF3_50, [0.914, 0.800, 0.790]
+color color_gb1_AF3_50, /gb1_AF3//A/51
+set_color color_gb1_AF3_51, [0.932, 0.892, 0.889]
+color color_gb1_AF3_51, /gb1_AF3//A/52
+set_color color_gb1_AF3_52, [0.923, 0.845, 0.839]
+color color_gb1_AF3_52, /gb1_AF3//A/53
+set_color color_gb1_AF3_53, [0.932, 0.892, 0.889]
+color color_gb1_AF3_53, /gb1_AF3//A/54
+set_color color_gb1_AF3_54, [0.923, 0.845, 0.839]
+color color_gb1_AF3_54, /gb1_AF3//A/55
+set_color color_gb1_AF3_55, [0.914, 0.800, 0.790]
+color color_gb1_AF3_55, /gb1_AF3//A/56
+set_color fad_color, [0.941, 0.941, 0.941]
+color fad_color, resn FAD
+set_view (        0.616044521,   -0.154009759,   -0.772509336,        -0.770424008,    0.086530536,   -0.631632686,        0.164123386,    0.984273493,   -0.065346345,        0.000000000,    0.000000000,  -89.381698608,        1.141838074,   -2.453596115,    1.223610878,        70.469207764,  108.294189453,  -20.000000000 )
+set ray_opaque_background, off
+set specular, 0
+set ray_trace_fog, 0
+ray
+png ray_color_gb1_Aligned_ESM2.png, dpi=300
